a875b3c5a4c044ad98fa0e9c77233c4d54cdcdfd
Pronoy Mandal <lukex9442@gmail.com>
2018-11-06 09:59:51 +0530

    Update top_module.vhd

--- PN_Sequence_Generator/top_module.vhd
+++ PN_Sequence_Generator/top_module.vhd
@@ -8,7 +8,7 @@ end top_module;
 
 architecture hierarchy of top_module is
 
-Signal Q1,Q2,Q3,Q4: STD_LOGIC:='0';                           --Intermediate outputs from individual flip flops
+Signal Q1,Q2,Q3,Q4: STD_LOGIC:='0';                      --Intermediate outputs from individual flip flops
 Signal x: STD_LOGIC:='0';
 
 component logic port ( D,clk,reset : in STD_LOGIC;       --Component representing D flip flop
@@ -20,7 +20,7 @@ end component;
 
 begin
 
-D1:logic port map(x,clk,rst,Q1);                        --Cascading the required flip flops to form an LFSR
+D1:logic port map(x,clk,rst,Q1);                         --Cascading the required flip flops to form an LFSR
 D2:logic port map(Q1,clk,rst,Q2);
 D3:logic port map(Q2,clk,rst,Q3);
 D4:logic port map(Q3,clk,rst,Q4);
